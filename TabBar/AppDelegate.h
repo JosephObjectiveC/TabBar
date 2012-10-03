f0a222014259ad4133f5626c4addbc49d61b5d69
Paul <paul@MacBook-Air-de-Paul.local>
2012-10-03 16:37:57 +0200

    Initial Commit

---
+++ TabBar/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  TabBar
+//
+//  Created by Paul on 03/10/12.
+//  Copyright (c) 2012 Paul. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
